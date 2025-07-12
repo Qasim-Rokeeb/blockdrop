a96e348fea8fef8ff872e78ef3d34a1b655f5988
Rokeeb Qasim <qasimrokeeb@gmail.com>
2025-07-12 15:58:26 +0000

    updated farcaster.json

--- src/app/.well-known/farcaster.json/route.ts
+++ src/app/.well-known/farcaster.json/route.ts
@@ -2,13 +2,12 @@ export async function GET() {
     const appUrl = process.env.NEXT_PUBLIC_URL;
   
     const config = {
-      accountAssociation: {
-        header:
-          "",
-        payload: "",
-        signature:
-          "",
-      },
+     "accountAssociation": {
+          "header": "eyJmaWQiOjg3MjkzNSwidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweEFEMDYzMjVEMjExODhEQ2FlMEJFRDMxODQ0ZDg3MjllQ0Y3MDQxRGIifQ",
+          "payload": "eyJkb21haW4iOiJibG9ja2Ryb3AtbmluZS52ZXJjZWwuYXBwIn0",
+          "signature": "MHhiM2M4YmVmYWNiMTg3YzZjNGQyNjViNjFlMDliZWYwNDNmZGI1ZDg0OGRjZDBhYjY0ZmNlYzk5MDk2OWM4Yzg1N2EzZTUzODFmYmZjYzRkYmQ4NDk2OWQwZTIxMmViNTA3YjNmMDIwMTE1MWFkMGQzYTM5OThlYmJiMTMxN2YxZDFj"
+        },
+
       frame: {
         version: "1",
         name: "BlockDrop",
